82aa6e4a4695d85348e2cc0c817b9c5ab20f7bd2
Firoz <mdfirozislam940@gmail.com>
2025-01-18 00:34:11 +0600

    second Commit

--- Hotel_Book/settings.py
+++ Hotel_Book/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['hotel-booking-gck0.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
